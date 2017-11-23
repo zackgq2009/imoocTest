61ca24ec701b8cdeb8700089954371effaaed5b4
johnnyKuo <zackgq@hotmail.com>
2017-11-23 11:59:14 +0800

    add webserver plugins in pom.xml

---
+++ JDBCTest/tests/com/tsingkuo/jdbcTest/GoddessViewTest.java
@@ -0,0 +1,33 @@
+package com.tsingkuo.jdbcTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by johnnykuo on 2017/11/20.
+ */
+public class GoddessViewTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void main() throws Exception {
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("");
+    }
+
+}
